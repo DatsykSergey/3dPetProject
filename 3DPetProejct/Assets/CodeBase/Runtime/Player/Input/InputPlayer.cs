24459e379ac134d895db19c33903e5439bc93386
SergeyDatsyk <s.dacyk@coderaptor.org>
2023-02-16 13:55:30 +0900

    Player input system

---
+++ 3DPetProejct/Assets/CodeBase/Runtime/Player/Input/InputPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Runtime.Player.Input
+{
+  public class InputPlayer
+  {
+    private PlayerActions _playerActions;
+    private Transform _camera;
+
+    public void SetPlayerCamera(Transform camera)
+    {
+      _camera = camera;
+    }
+
+    public Vector2 GetMoveDirection()
+    {
+      return _playerActions.Gameplay.MoveDirection.ReadValue<Vector2>();
+    }
+    
+    
+  }
+}
