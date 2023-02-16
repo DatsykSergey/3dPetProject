8f21768214eee0ef6fa1156506b0e07ab8505324
SergeyDatsyk <s.dacyk@coderaptor.org>
2023-02-16 14:27:01 +0900

    Application installer

---
+++ 3DPetProejct/Assets/CodeBase/Runtime/Infrastructure/Installers/ApplicationInstaller.cs
@@ -0,0 +1,16 @@
+﻿using CodeBase.Runtime.Infrastructure.Services.Input;
+using Zenject;
+
+namespace CodeBase.Runtime.Infrastructure.Installers
+{
+  public class ApplicationInstaller : MonoInstaller
+  {
+    public override void InstallBindings()
+    {
+      Container
+        .Bind<IPlayerInput>()
+        .To<PlayerInput>()
+        .AsSingle();
+    }
+  }
+}
