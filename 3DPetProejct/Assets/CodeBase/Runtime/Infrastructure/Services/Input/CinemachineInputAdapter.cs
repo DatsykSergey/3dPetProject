a3d9ab4f69b4bf873ccb5a9081ba6ef7f45d6d5d
SergeyDatsyk <min3drav1@gmail.com>
2023-02-18 16:17:53 +0900

    Add cinemachine adapter for new input system

---
+++ 3DPetProejct/Assets/CodeBase/Runtime/Infrastructure/Services/Input/CinemachineInputAdapter.cs
@@ -0,0 +1,26 @@
+﻿using Cinemachine;
+using UnityEngine;
+using Zenject;
+
+namespace CodeBase.Runtime.Infrastructure.Services.Input
+{
+  public class CinemachineInputAdapter : MonoBehaviour
+  {
+    [SerializeField] private CinemachineFreeLook _freeLook;
+    private IPlayerInput _input;
+
+    [Inject]
+    private void Construct(IPlayerInput input)
+    {
+      _input = input;
+    }
+
+    private void LateUpdate()
+    {
+      Vector2 lookDelta = _input.GetLookDelta();
+      _freeLook.m_XAxis.Value += lookDelta.x;
+      lookDelta.Normalize();
+      _freeLook.m_YAxis.Value -= lookDelta.y / 100;
+    }
+  }
+}
