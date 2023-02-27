15216f1e34836d69e90f051cab8129279d2dc763
SergeyDatsyk <min3drav1@gmail.com>
2023-02-28 02:37:50 +0900

    Test animation

---
+++ 3DPetProejct/Assets/CodeBase/GameplayTest/Animation/TestAnimation.cs
@@ -0,0 +1,17 @@
+﻿using CodeBase.Runtime.Gameplay.Character;
+using UnityEngine;
+
+namespace CodeBase.GameplayTest.Animation
+{
+  public class TestAnimation : MonoBehaviour
+  {
+    [SerializeField, Range(-1, 1)] private float _vertical;
+    [SerializeField, Range(-1, 1)] private float _horizontal;
+    [SerializeField] private CharacterAnimator _animator;
+    
+    private void Update()
+    {
+      _animator.UpdateMovement(_vertical, _horizontal);
+    }
+  }
+}
