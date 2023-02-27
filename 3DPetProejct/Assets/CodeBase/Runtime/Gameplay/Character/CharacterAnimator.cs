9d6389ca13f9a16e6f98cd8365ce6007cfb3d32e
SergeyDatsyk <min3drav1@gmail.com>
2023-02-28 02:37:37 +0900

    Character animator

---
+++ 3DPetProejct/Assets/CodeBase/Runtime/Gameplay/Character/CharacterAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Runtime.Gameplay.Character
+{
+  public class CharacterAnimator : MonoBehaviour
+  {
+    [SerializeField] private Animator _animator;
+
+    private readonly int _verticalId = Animator.StringToHash("VerticalMovement");
+    private readonly int _horizontalId = Animator.StringToHash("HorizontalMovement");
+
+
+    public void UpdateMovement(float vertical, float horizontal)
+    {
+      _animator.SetFloat(_verticalId, vertical);
+      _animator.SetFloat(_horizontalId, horizontal);
+    }
+  }
+}
