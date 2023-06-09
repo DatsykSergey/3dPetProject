6a3903699ea2d6e4f0de98a0738411179bc969ee
SergeyD <min3drav1@gmail.com>
2023-06-09 22:25:23 +0900

    LedgeDetector

--- 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/LedgeDetector.cs
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/LedgeDetector.cs
@@ -1,4 +1,5 @@
-﻿using Core.CodeBase.Runtime.DebugTools.CustomGizmos;
+﻿using System.Linq;
+using Core.CodeBase.Runtime.DebugTools.CustomGizmos;
 using UnityEngine;
 
 namespace Core.CodeBase.Runtime.Gameplay
@@ -8,6 +9,7 @@ namespace Core.CodeBase.Runtime.Gameplay
     [SerializeField] private Vector3 _boxSize = Vector3.one;
     [SerializeField] private LayerMask _layerMask;
     private readonly Collider[] _result = new Collider[8];
+    private int _count = 0;
 
 
     private void FixedUpdate()
@@ -17,13 +19,37 @@ namespace Core.CodeBase.Runtime.Gameplay
         return;
       }
 
-      int count = Physics.OverlapBoxNonAlloc(transform.position, _boxSize, _result, transform.rotation, _layerMask,
+      _count = Physics.OverlapBoxNonAlloc(transform.position, _boxSize, _result, transform.rotation, _layerMask,
         QueryTriggerInteraction.UseGlobal);
 
-      for (int i = 0; i < count; i++)
+
+    }
+
+    private void Update()
+    {
+      for (int i = 0; i < _count; i++)
       {
-        CustomGizmos.Isntance.DrawSphere(_result[i].transform.position, 0.5f, Color.green);
+        BoxCollider boxCollider = _result[i] as BoxCollider;
+        if (boxCollider == null)
+        {
+          Debug.LogWarning("Find not box collider Ledge");
+          continue;
+        }
+        CustomGizmos.Isntance.DrawSphere(boxCollider.transform.position, 0.5f, Color.green);
+        Vector3 rightPoint = GetRightPoint(boxCollider);
+        Vector3 leftPoint = GetLeftPoint(boxCollider);
+        CustomGizmos.Isntance.DrawLine(rightPoint, leftPoint, Color.green);
       }
+    }
+
+    private static Vector3 GetRightPoint(BoxCollider boxCollider)
+    {
+      return boxCollider.transform.position + boxCollider.transform.right * (boxCollider.size.x * 0.5f);
+    }
+
+    private static Vector3 GetLeftPoint(BoxCollider boxCollider)
+    {
+      return boxCollider.transform.position - boxCollider.transform.right * (boxCollider.size.x * 0.5f);
     }
 
     private void OnDrawGizmos()
