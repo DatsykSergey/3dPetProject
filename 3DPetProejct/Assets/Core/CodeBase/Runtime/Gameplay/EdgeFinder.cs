1ad2d71379938ed3716e0aad1ed0db5baff933b6
SergeyD <min3drav1@gmail.com>
2023-06-13 18:52:34 +0900

    GrabEdge

--- 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/EdgeFinder.cs
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/EdgeFinder.cs
@@ -8,17 +8,26 @@ namespace Core.CodeBase.Runtime.Gameplay
   {
     [SerializeField] private BaseObstacleChecker _forward;
     [SerializeField] private BaseObstacleChecker _up;
-
+    
+    public bool IsHasPoint { get; private set; }
+    public Vector3 EdgePoint { get; private set; }
+    
     private void Update()
     {
       if (_up.HasObstacle == false || _forward.HasObstacle == false)
       {
+        IsHasPoint = false;
         return;
       }
 
-      Vector3 edgePoint = _forward.HitPoint;
-      edgePoint.y = _up.HitPoint.y;
-      CustomGizmos.Instance.DrawSphere(edgePoint, 0.2f, Color.green);
+      EdgePoint = GetEdgePoint();
+      IsHasPoint = true;
+      CustomGizmos.Instance.DrawSphere(EdgePoint, 0.2f, Color.green);
+    }
+
+    private Vector3 GetEdgePoint()
+    {
+      return new Vector3(_forward.HitPoint.x, _up.HitPoint.y, _forward.HitPoint.z);
     }
   }
 }
---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/EdgeGrab.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using Core.CodeBase.Runtime.Gameplay.Player;
+using Core.CodeBase.Runtime.Infrastructure.Services.Input;
+using UnityEngine;
+using Zenject;
+
+namespace Core.CodeBase.Runtime.Gameplay
+{
+  public class EdgeGrab : MonoBehaviour
+  {
+    [SerializeField] private EdgeFinder _edgeFinder;
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] private Transform _grabTransform;
+  
+    private EdgeGrabState _currentState = EdgeGrabState.None;
+    private IPlayerInput _playerInput;
+
+    private enum EdgeGrabState
+    {
+      None = 0,
+      Grabbed = 1,
+      Move = 2,
+    }
+
+    [Inject]
+    private void Construct(IPlayerInput playerInput)
+    {
+      _playerInput = playerInput;
+    }
+
+    private void Update()
+    {
+      if (_currentState == EdgeGrabState.None &&
+          _playerMovement.IsGrounded == false && 
+          _edgeFinder.IsHasPoint)
+      {
+        GrabEdge(_edgeFinder.EdgePoint);
+      }
+
+      if (_currentState == EdgeGrabState.Grabbed && 
+          (_edgeFinder.IsHasPoint == false || _playerInput.IsPressJumpDown()))
+      {
+        JumpDown();
+      }
+    }
+
+    private void GrabEdge(Vector3 point)
+    {
+      Vector3 shit = point - _grabTransform.position;
+      _playerMovement.FreezeMovement();
+      _playerMovement.transform.position += shit;
+      _currentState = EdgeGrabState.Grabbed;
+    }
+
+    private void JumpDown()
+    {
+      StopAllCoroutines();
+      StartCoroutine(StartJumpDown());
+    }
+
+    private IEnumerator StartJumpDown()
+    {
+      _playerMovement.UnfreezeMovement();
+      _currentState = EdgeGrabState.Move;
+      yield return new WaitForSeconds(0.5f);
+      _currentState = EdgeGrabState.None;
+    }
+  }
+}
