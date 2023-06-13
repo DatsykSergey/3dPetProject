efe4a5a19e9ace899aaa82db4393311aa6efb3fb
SergeyD <min3drav1@gmail.com>
2023-06-13 18:10:04 +0900

    Add base obstacle checker

---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/BaseObstacleChecker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core.CodeBase.Runtime.Gameplay
+{
+  public class BaseObstacleChecker : MonoBehaviour
+  {
+    [SerializeField] protected float _castDistance = 1f;
+    [SerializeField] protected LayerMask _layerMask;
+    protected RaycastHit _hits;
+    [field:SerializeField] public bool HasObstacle { get; protected set; }
+    public Vector3 HitPoint => _hits.point;
+    public Vector3 HitNormal => _hits.normal;
+  }
+}
--- 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/ObstacleCheckerBySphere.cs
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/ObstacleCheckerBySphere.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using Core.CodeBase.Runtime.DebugTools.CustomGizmos;
+using UnityEditor;
 using UnityEngine;
 
 namespace Core.CodeBase.Runtime.Gameplay
 {
-  public class ObstacleChecker : MonoBehaviour
+  public class ObstacleCheckerBySphere : BaseObstacleChecker
   {
-    [SerializeField] private float _castRadius;
-    [SerializeField] private float _castDistance;
-    [SerializeField] private LayerMask _layerMask;
-
-    private RaycastHit _hits;
-    [field:SerializeField] public bool HasObstacle { get; private set; }
-    public Vector3 HitPoint => _hits.point;
-    public Vector3 HitNormal => _hits.normal;
+    [SerializeField] private float _castRadius = 0.25f;
 
     private void FixedUpdate()
     {
       Ray ray = new Ray(transform.position, transform.forward);
       HasObstacle = Physics.SphereCast(ray, _castRadius, out _hits, _castDistance, _layerMask);
-      // HasObstacle = Physics.Raycast(transform.position,transform.forward, out _hits, _castDistance, _layerMask);
     }
 
     private void Update()
     {
       Color color = HasObstacle ? Color.red : Color.blue;
       CustomGizmos.Instance.DrawSphereCast(transform.position, transform.forward, _castRadius, HasObstacle ? _hits.distance : _castDistance, color);
+    }
+
+    private void OnDrawGizmos()
+    {
+      if (EditorApplication.isPlaying == true)
+      {
+        return;
+      }
+      
+      Gizmos.color = Color.blue;
+      Gizmos.DrawSphere(transform.position, _castRadius);
+      Gizmos.DrawLine(transform.position, transform.position + transform.forward * _castDistance);
     }
   }
 }
