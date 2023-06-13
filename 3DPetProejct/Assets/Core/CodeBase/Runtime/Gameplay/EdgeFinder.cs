e895f0c10a94f352991d5cc94bc21f959ab51937
SergeyD <min3drav1@gmail.com>
2023-06-13 18:11:24 +0900

    EdgeFinder

---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Gameplay/EdgeFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Core.CodeBase.Runtime.DebugTools.CustomGizmos;
+using UnityEngine;
+
+namespace Core.CodeBase.Runtime.Gameplay
+{
+  public class EdgeFinder : MonoBehaviour
+  {
+    [SerializeField] private BaseObstacleChecker _forward;
+    [SerializeField] private BaseObstacleChecker _up;
+
+    private void Update()
+    {
+      if (_up.HasObstacle == false || _forward.HasObstacle == false)
+      {
+        return;
+      }
+
+      Vector3 edgePoint = _forward.HitPoint;
+      edgePoint.y = _up.HitPoint.y;
+      CustomGizmos.Instance.DrawSphere(edgePoint, 0.2f, Color.green);
+    }
+  }
+}
