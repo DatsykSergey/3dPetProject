e354cd21894f62be6de554dd3f69939df665f0f2
SergeyDatsyk <min3drav1@gmail.com>
2023-10-03 01:29:21 +0900

    PlayablePlayerAnimator

---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Animations/Custom/PlayablePlayerAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace Core.CodeBase.Runtime.Animations.Custom
+{
+  [AddComponentMenu("My animation/Playable animation")]
+  public class PlayablePlayerAnimator : BasePlayerAnimator
+  {
+    [SerializeField] private Animator _animator;
+    [Header("Clips")] [SerializeField] private AnimationClip _idleClip;
+
+    private PlayableGraph _playableGraph;
+
+    private void Awake()
+    {
+      _playableGraph = PlayableGraph.Create();
+      _playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+      AnimationPlayableOutput output = AnimationPlayableOutput.Create(_playableGraph, "GraphPlayerAnimator", _animator);
+      AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(_playableGraph, _idleClip);
+      output.SetSourcePlayable(clipPlayable);
+      _playableGraph.Play();
+    }
+
+    private void OnDestroy()
+    {
+      _playableGraph.Destroy();
+    }
+
+    public override void UpdateMovement(Vector3 velocity)
+    {
+    }
+  }
+}
