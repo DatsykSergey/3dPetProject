f5b51c781ba86a3a10ffd753932eb7cb7e4d11c7
SergeyDatsyk <min3drav1@gmail.com>
2023-10-10 01:50:32 +0900

    Animator callback

---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Animations/AnimatorCallback.cs
@@ -0,0 +1,17 @@
+﻿using Core.CodeBase.Runtime.Gameplay.Player;
+using UnityEngine;
+
+namespace Core.CodeBase.Runtime.Animations
+{
+  public class AnimatorCallback : MonoBehaviour
+  {
+    [SerializeField] private PlayerMovement _movement;
+    [SerializeField] private BasePlayerAnimator _animator;
+    
+    public void JumpUp()
+    {
+      _movement.JumpUp();  
+      _animator?.JumpReset();
+    }
+  }
+}
