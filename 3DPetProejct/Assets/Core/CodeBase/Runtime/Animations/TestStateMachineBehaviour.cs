e296c375882db879507e98b8474e018e15879031
SergeyDatsyk <min3drav1@gmail.com>
2023-10-12 04:36:21 +0900

    TestStateMachineBehaviour

---
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/Animations/TestStateMachineBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Core.CodeBase.Runtime.Animations
+{
+  public class TestStateMachineBehaviour : StateMachineBehaviour
+  {
+
+    [SerializeField] private Vector3 _offset;
+    private Vector3 _position;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+      _position = animator.transform.localPosition;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+      animator.transform.localPosition = _position + _offset;
+    }
+    
+
+  }
+}
