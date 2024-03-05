d6c16b80f204cd2cbcb7f163d38c6fb8b8c3d151
SergeyDatsyk <min3drav1@gmail.com>
2024-03-06 03:04:40 +0900

    Create character data for fsm

--- 3DPetProejct/Assets/Core/CodeBase/Runtime/CharacterFSM/CharacterData.cs
+++ 3DPetProejct/Assets/Core/CodeBase/Runtime/CharacterFSM/CharacterData.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Core.CodeBase.Runtime.CharacterFSM
 {
-  [Serializable]
-  public class CharacterData
+  [CreateAssetMenu( menuName = "Data/CharacteFsmData")]
+  public class CharacterData : ScriptableObject
   {
-    
+    [field: SerializeField] public MovementData Movement { get; private set; }
+  }
+
+  [Serializable]
+  public class MovementData
+  {
+    [field:SerializeField] public float Speed { get; private set; }
+    [field:SerializeField] public float RotationSpeed { get; private set; }
   }
 }
